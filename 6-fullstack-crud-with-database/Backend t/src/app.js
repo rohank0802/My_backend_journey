bde4d5dcbd5e8f89d6069aacde200b176413ad8b
Rohan Kushwaha <rohankushwaha351@gmail.com>
2026-05-04 15:11:43 +0530

    created one server of backend and frontend for deployment

--- 6-fullstack-crud-with-database/Backend t/src/app.js
+++ 6-fullstack-crud-with-database/Backend t/src/app.js
@@ -1,7 +1,12 @@
 const express=require("express")
 const productModel=require("./model/product.model")
-const cors=require("cors")
 const app=express()
+const cors=require("cors")
+// this,path,stect is use for backend serves frontend means to make single server deployment
+const path=require("path")
+app.use(express.static("./public"))
+
+
 app.use(express.json())
 app.use(cors())///with this we are allwing cors means we are allowing cross browser access .Note this will we only done for development purpose .in real production app we will not do this.
 //post method
@@ -42,6 +47,10 @@ res.status(200).json({
     res.status(200).json({
           message:"product updated successfully",
       })
+ })
+//after that if i gave any path that is not in public folder then we will send index.html folder
+ app.get('*name',(req,res)=>{
+    res.sendFile(path.join(__dirname,"..","/public/index.html"))
  })
 
 module.exports=app
