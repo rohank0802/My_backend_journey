015c3c3c5c7ebaf7509798b50de6a0df9c43c7fd
Rohan Kushwaha <rohankushwaha351@gmail.com>
2026-06-24 18:05:48 +0530

    added googles Oauth2 to sent email by server the use lancgain features like tool,createAgent where LLM agent will will sent email to person i want to send,also i use mistralai agent in this.

--- 13-Gen-Ai/index.js
+++ 13-Gen-Ai/index.js
@@ -1,7 +1,22 @@
 import "dotenv/config"
 import readline from "readline/promises"
 import { ChatMistralAI } from "@langchain/mistralai"
-import {HumanMessage} from "langchain"
+import {HumanMessage,tool,createAgent} from "langchain"
+import {sendEmail} from "./mail.service.js"
+import * as z from "zod"
+const emailTool=tool(
+    sendEmail,
+    {
+        name:"emailTool",
+        description:"use this tool to send an email.",
+        schema:z.object({
+            to:z.string().describe("the recipient,s email address"),
+            html:z.string().describe("the HTML content of the email"),
+            subject:z.string().describe("the subject of the email")
+        })
+    }
+)
+
 const rl=readline.createInterface({
     input:process.stdin,
     output:process.stdout,
@@ -12,19 +27,28 @@ const rl=readline.createInterface({
 // rl.close()
 //})
 console.log()
-const llm = new ChatMistralAI({
+const model = new ChatMistralAI({
     model: "mistral-small-latest",
    
-    // other params...
 })
+// know we will create aget to used llm and tool together
+const agent=createAgent({
+    model,
+    tools:[emailTool]
+})
+
 const message=[]
 while (true){
     const userInput=await rl.question("you:")
     message.push(new HumanMessage(userInput))
 
-    const response=await llm.invoke(message)
-    message.push(response)
-    console.log(` AI:${response.content}`)
+    const response=await agent.invoke({messages:message})
+    message.push(response.messages[ response.messages.length - 1 ])
+    
+     console.log(`\x1b[34m[AI]\x1b[0m ${response.messages[ response.messages.length - 1 ].content}`)
+    // message.push(response)
+    // console.log(response.messages[response.messages.length-1])
+    // console.log(` AI:${response.content}`)
 }
 
 
