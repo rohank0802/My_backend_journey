17c9dc62cf6e6fce1d5a039feac8e45b3b7bcc71
Rohan Kushwaha <rohankushwaha351@gmail.com>
2026-07-19 16:35:37 +0530

    second commit applied  3 diffrent llms first 2 llms are for giving the solution and 3rd llm is for giving score,acc  to the fitst 2 llms solution on the same question,use langgraph and langchain .

--- 15-ModelArena with langgraph/Backend/src/services/graph.ai.service.ts
+++ 15-ModelArena with langgraph/Backend/src/services/graph.ai.service.ts
@@ -1,27 +1,80 @@
-import { StateSchema, MessagesValue, StateGraph, START, END } from "@langchain/langgraph"
+import { StateSchema, MessagesValue,ReducedValue,type GraphNode, StateGraph, START, END } from "@langchain/langgraph"
 
-type JUDGEMENT={
-    winner:"solution_1"|"solution_2";
-    solution_1_score:number;
-    solution_2_score:number;
+import { mistralModel,cohereModel, geminiModel  } from "./models.service.js";
+import {createAgent,providerStrategy} from "langchain"
+import {z} from "zod"
+import { HumanMessage } from "@langchain/core/messages";
 
+const State=new StateSchema({
+    messages:MessagesValue,
+    solution_1:new ReducedValue(z.string().default(""),{
+        reducer:(current,next)=>{
+            return next
+        }
+    }),
+     solution_2:new ReducedValue(z.string().default(""),{
+        reducer:(current,next)=>{
+            return next
+        }
+    }),
+    judge_recomendation:new ReducedValue(z.object({
+        solution_1_score:z.number().default(0),
+        solution_2_score:z.number().default(0),
+    }),
+    {reducer:(getCurrentTaskInput,next)=>{
+        return next
+    }}
+)
+})
+
+const solutionNode:GraphNode<typeof State>=async(state:typeof State)=>{
+ const [mistral_solution,cohere_solution]=await Promise.all([
+    mistralModel.invoke(state.messages[0].text),
+    cohereModel.invoke(state.messages[0].text)
+ ])
+ return{
+    solution_1:mistral_solution.text,
+    solution_2:cohere_solution.text
+ }
 }
 
-type AIBATTLESTATE={
-    messages: typeof MessagesValue;
-   solution_1:string;
-    solution_2:string;
-    judgement:JUDGEMENT;
-}
-const state:AIBATTLESTATE={
-messages:MessagesValue,
-solution_1:"",
-solution_2:"",
-judgement:{
-    winner:"solution_1",
-    solution_1_score:0,
-    solution_2_score:0
+const judgeNode:GraphNode<typeof State>=async(state:typeof State)=>{
+    const {solution_1 ,solution_2}=state
+ const judge=createAgent({
+    model:geminiModel,
+    tools:[],
+    responseFormat:providerStrategy(z.object({
+        solution_1_score:z.number().min(0).max(10),
+        solution_2_score:z.number().min(0).max(10)
+    }))
+ })
+ const judgeResponse=await judge.invoke({
+    messages:[
+    new HumanMessage(`you are a judge with evaluating the quality of two solutions to a problem. The problem is:${state.messages[0].text}.the first solution is:${solution_1}.the second solution is:${solution_2},Please privide a score between 0 and 10 for each solutionNode, where 0 means solution is completely incorrect or irrelevent,and 10 means the solution is perfect and fully adresses the problem.`)
+ ]
+ })
+ const result=judgeResponse.structuredResponse
 
+ return{
+    judge_recomendation:result
+ }
 }
 
+const graph=new StateGraph(State)
+.addNode("solution",solutionNode)
+.addNode("judge",judgeNode)
+.addEdge(START,"solution")
+.addEdge("solution","judge")
+.addEdge("judge",END)
+.compile()
+
+
+export default async function(userMessage:string) {
+    const result=await graph.invoke({
+        messages:[
+            new HumanMessage(userMessage)
+        ]
+    })
+console.log(result)
+    return result.messages
 }
